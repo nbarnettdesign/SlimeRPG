9a057d98570d9519d2458afeff3177663163f2ff
Nick Barnett <nick.barnett@twobulls.com>
2019-05-21 16:13:36 +1000

    First Push: Step 6
    Getting there, i shouldn’t have added so many assets though

---
+++ Assets/ItemListCSV.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class ItemListCSV : MonoBehaviour
+{
+
+    public TextAsset itemListCSV;
+
+    private char lineSeperater = '\n';
+    private char fieldSeperator = ',';
+    private string[] records;
+    private string[] fields;
+
+
+
+    void Start()
+    {
+
+        ReadData();
+    }
+    // Read data from CSV file
+    private void ReadData()
+    {
+        records = itemListCSV.text.Split(lineSeperater);
+        foreach (string record in records)
+        {
+            fields = record.Split(fieldSeperator);
+            foreach (string field in fields)
+            {
+                //contentArea.text += field + "\t";
+                FuckYaChickenStrips();
+            }
+            int num_rows = fields.Length;
+            int num_cols = fields[0].Split(',').Length;
+            Debug.Log("rows:" + num_rows + " columns:" + num_cols);
+            //contentArea.text += '\n';
+        }
+    }
+
+    public void FuckYaChickenStrips()
+    {
+        ScriptableObject.CreateInstance<Equipment>();
+        //ScriptableObjectUtility.CreateAsset<ItemList>();
+    }
+
+}
---
+++ Assets/TransformButtonOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransformButtonOptions : MonoBehaviour
+{
+    public GameObject gameManger;
+    public GameObject humanButton;
+    public GameObject skeletonButton;
+
+
+    private void OnEnable()
+    {
+        if (gameManger.GetComponent<PlayerExp>().humanLevel > 0)
+        {
+            humanButton.SetActive(true);
+        }
+        else humanButton.SetActive(false);
+
+        if (gameManger.GetComponent<PlayerExp>().skeletonLevel > 0)
+        {
+            skeletonButton.SetActive(true);
+        }
+        else skeletonButton.SetActive(false);
+    }
+}
